782f9772e7b7e2a82caedbe9afa14ead6a9a02ee
Danil Ponomarenko <el.pochtadp@gmail.com>
2026-07-01 22:49:47 +0300

    Решение №3

--- m6_t3_zadacha_1__ustanovka/src/Main.java
+++ m6_t3_zadacha_1__ustanovka/src/Main.java
@@ -5,69 +5,33 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    static public final String FILE_PATH = "/Users/ponomarenko/learning/actual/java/netology/classwork/mod_6_java_core/t3_potoki_vvoda_vyvoda_rabota_s_faylami_serializatsiya/ver4/m6_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/m6_t3_zadacha_1__ustanovka";
+
     static void main(String[] args) {
         StringBuilder log = new StringBuilder();
         String fullPath = "C:/Users/d.ponomarenko/work/napravleniya_raboty/my_personal_dannue/java/m4_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/ver3/m6_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/m6_t3_zadacha_1__ustanovka";
 
         List<String> directories = new ArrayList<>(List.of(
-                fullPath + "/Games/src",
-                fullPath + "/Games/res",
-                fullPath + "/Games/savegames",
-                fullPath + "/Games/temp"
+                "/Games/src",
+                "/Games/res",
+                "/Games/savegames",
+                "/Games/temp",
+                "/Games/src/main",
+                "/Games/src/test",
+                "/Games/res/drawables",
+                "/Games/res/vectors",
+                "/Games/res/icons"
         ));
 
-        createDirectory(log, fullPath + "/Games/src");
-
-        // 1. В папке `Games` создайте несколько директорий: `src`, `res`, `savegames`, `temp`.
         for (String directori : directories) {
             createDirectory(log, directori);
         }
 
-        // 2. В каталоге `src` создайте две директории: `main`, `test`.
-        createDirectory(log, fullPath + "/Games/src/main");
-        createDirectory(log, fullPath + "/Games/src/test");
+        createFile(log, "/Games/src/main", "Main.java");
+        createFile(log, "/Games/src/main", "Utils.java");
+        createFile(log, "/Games/temp", "temp.txt");
 
-        // 3. В подкаталоге `main` создайте два файла: `Main.java`, `Utils.java`.
-        File[] childsSrcMain = {
-                new File(fullPath + "/Games/src/main/Main.java"),
-                new File(fullPath + "/Games/src/main/Utils.java")
-        };
-
-        for (File child : childsSrcMain) {
-            try {
-                if (child.createNewFile()) {
-                    log.append("Файл " + child.getName() + " создан в " + child.getPath() + "\n");
-                } else {
-                    log.append("Файл " + child.getName() + " НЕ создан в " + child.getPath() + "\n");
-                }
-            } catch (IOException exception) {
-                System.out.println(exception.getMessage());
-            }
-        }
-
-        // 4. В каталог `res` создайте три директории: `drawables`, `vectors`, `icons`.
-        createDirectory(log, fullPath + "/Games/res/drawables");
-        createDirectory(log, fullPath + "/Games/res/vectors");
-        createDirectory(log, fullPath + "/Games/res/icons");
-
-        // 5. В директории `temp` создайте файл `temp.txt`.
-        File[] childstemp = {
-                new File(fullPath + "/Games/temp", "temp.txt")
-        };
-
-        for (File child : childstemp) {
-            try {
-                if (child.createNewFile()) {
-                    log.append("Файл " + child.getName() + " создан в " + child.getPath() + "\n");
-                } else {
-                    log.append("Файл " + child.getName() + " НЕ создан в " + child.getPath() + "\n");
-                }
-            } catch (IOException exception) {
-                System.out.println(exception.getMessage());
-            }
-        }
-
-        try (FileWriter writerLog = new FileWriter(fullPath + "/Games/temp/temp.txt", false)) {
+        try (FileWriter writerLog = new FileWriter(FILE_PATH + "/Games/temp/temp.txt", false)) {
             writerLog.write(log.toString());
             writerLog.flush();
         } catch (IOException exception) {
@@ -76,12 +40,26 @@ public class Main {
     }
 
     public static void createDirectory(StringBuilder log, String path) {
-        File file = new File(path);
+        File file = new File(FILE_PATH + path);
 
         if (file.mkdir()) {
             log.append("Каталог " + file.getName() + " создан в " + file.getPath() + "\n");
         } else {
             log.append("Каталог " + file.getName() + " НЕ создан в " + file.getPath() + "\n");
+        }
+    }
+
+    public static void createFile(StringBuilder log, String path, String name) {
+        File file = new File(FILE_PATH + path, name);
+
+        try {
+            if (file.createNewFile()) {
+                log.append("Файл " + file.getName() + " создан в " + file.getPath() + "\n");
+            } else {
+                log.append("Файл " + file.getName() + " НЕ создан в " + file.getPath() + "\n");
+            }
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
         }
     }
 }
--- m6_t3_zadacha_2__sokhranenie/src/Main.java
+++ m6_t3_zadacha_2__sokhranenie/src/Main.java
@@ -1,23 +1,23 @@
 import java.util.List;
 
 public class Main {
+    static public final String FILE_PATH = "/Users/ponomarenko/learning/actual/java/netology/classwork/mod_6_java_core/t3_potoki_vvoda_vyvoda_rabota_s_faylami_serializatsiya/ver4/m6_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/m6_t3_zadacha_1__ustanovka/Games/savegames";
+
     static void main(String[] args) {
         GameProgress playerOne = new GameProgress(100, 80, 6, 40.0);
         GameProgress playerTwo = new GameProgress(64, 45, 8, 22.5);
         GameProgress playerThree = new GameProgress(20, 36, 2, 10.2);
 
-        String myPath = "C:/Users/d.ponomarenko/work/napravleniya_raboty/my_personal_dannue/java/m4_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/ver3/m6_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/m6_t3_zadacha_1__ustanovka/Games/savegames";
-
         Serializator serializator = new Serializator();
 
-        serializator.saveGame(myPath + "/playerOne.data", playerOne);
-        serializator.saveGame(myPath + "/playerTwo.data", playerTwo);
-        serializator.saveGame(myPath + "/playerThree.data", playerThree);
+        serializator.saveGame(FILE_PATH + "/playerOne.data", playerOne);
+        serializator.saveGame(FILE_PATH + "/playerTwo.data", playerTwo);
+        serializator.saveGame(FILE_PATH + "/playerThree.data", playerThree);
 
-        serializator.zipFiles(myPath + "/players.zip", List.of(
-                myPath + "/playerOne.data",
-                myPath + "/playerTwo.data",
-                myPath + "/playerThree.data"
+        serializator.zipFiles(FILE_PATH + "/players.zip", List.of(
+                FILE_PATH + "/playerOne.data",
+                FILE_PATH + "/playerTwo.data",
+                FILE_PATH + "/playerThree.data"
         ));
     }
 }
