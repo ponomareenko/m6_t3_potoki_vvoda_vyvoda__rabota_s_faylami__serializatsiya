98af0359b0af2ef032d6ca8edfcb45082345501c
Danil Ponomarenko <el.pochtadp@gmail.com>
2026-07-01 12:18:29 +0300

    Update Main.java

--- m6_t3_zadacha_1__ustanovka/src/Main.java
+++ m6_t3_zadacha_1__ustanovka/src/Main.java
@@ -1,45 +1,36 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     static void main(String[] args) {
         StringBuilder log = new StringBuilder();
+        String fullPath = "C:/Users/d.ponomarenko/work/napravleniya_raboty/my_personal_dannue/java/m4_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/ver3/m6_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/m6_t3_zadacha_1__ustanovka";
 
-//        1. В папке `Games` создайте несколько директорий: `src`, `res`, `savegames`, `temp`.
-        File[] childsGames = {
-                new File("Games", "src"),
-                new File("Games", "res"),
-                new File("Games", "savegames"),
-                new File("Games", "temp")
-        };
+        List<String> directories = new ArrayList<>(List.of(
+                fullPath + "/Games/src",
+                fullPath + "/Games/res",
+                fullPath + "/Games/savegames",
+                fullPath + "/Games/temp"
+        ));
 
-        for (File child : childsGames) {
-            if (child.mkdir()) {
-                log.append("Каталог " + child.getName() + " создан в " + child.getPath() + "\n");
-            } else {
-                log.append("Каталог " + child.getName() + " НЕ создан в " + child.getPath() + "\n");
-            }
+        createDirectory(log, fullPath + "/Games/src");
+
+        // 1. В папке `Games` создайте несколько директорий: `src`, `res`, `savegames`, `temp`.
+        for (String directori : directories) {
+            createDirectory(log, directori);
         }
 
-//        2. В каталоге `src` создайте две директории: `main`, `test`.
-        File[] childsSrc = {
-                new File("Games/src", "main"),
-                new File("Games/src", "test")
-        };
+        // 2. В каталоге `src` создайте две директории: `main`, `test`.
+        createDirectory(log, fullPath + "/Games/src/main");
+        createDirectory(log, fullPath + "/Games/src/test");
 
-        for (File child : childsSrc) {
-            if (child.mkdir()) {
-                log.append("Подкаталог " + child.getName() + " создан в " + child.getPath() + "\n");
-            } else {
-                log.append("Подкаталог " + child.getName() + " НЕ создан в " + child.getPath() + "\n");
-            }
-        }
-
-//        3. В подкаталоге `main` создайте два файла: `Main.java`, `Utils.java`.
+        // 3. В подкаталоге `main` создайте два файла: `Main.java`, `Utils.java`.
         File[] childsSrcMain = {
-                new File("Games/src/main", "Main.java"),
-                new File("Games/src/main", "Utils.java")
+                new File(fullPath + "/Games/src/main/Main.java"),
+                new File(fullPath + "/Games/src/main/Utils.java")
         };
 
         for (File child : childsSrcMain) {
@@ -54,24 +45,14 @@ public class Main {
             }
         }
 
-//        4. В каталог `res` создайте три директории: `drawables`, `vectors`, `icons`.
-        File[] childRes = {
-                new File("Games/res", "drawables"),
-                new File("Games/res", "vectors"),
-                new File("Games/res", "icons"),
-        };
+        // 4. В каталог `res` создайте три директории: `drawables`, `vectors`, `icons`.
+        createDirectory(log, fullPath + "/Games/res/drawables");
+        createDirectory(log, fullPath + "/Games/res/vectors");
+        createDirectory(log, fullPath + "/Games/res/icons");
 
-        for (File child : childRes) {
-            if (child.mkdir()) {
-                log.append("Подкаталог " + child.getName() + " создан в " + child.getPath() + "\n");
-            } else {
-                log.append("Подкаталог " + child.getName() + " НЕ создан в " + child.getPath() + "\n");
-            }
-        }
-
-//        5. В директории `temp` создайте файл `temp.txt`.
+        // 5. В директории `temp` создайте файл `temp.txt`.
         File[] childstemp = {
-                new File("Games/temp", "temp.txt")
+                new File(fullPath + "/Games/temp", "temp.txt")
         };
 
         for (File child : childstemp) {
@@ -86,11 +67,21 @@ public class Main {
             }
         }
 
-        try (FileWriter writerLog = new FileWriter("Games/temp/temp.txt", false)) {
+        try (FileWriter writerLog = new FileWriter(fullPath + "/Games/temp/temp.txt", false)) {
             writerLog.write(log.toString());
             writerLog.flush();
         } catch (IOException exception) {
             System.out.println(exception.getMessage());
+        }
+    }
+
+    public static void createDirectory(StringBuilder log, String path) {
+        File file = new File(path);
+
+        if (file.mkdir()) {
+            log.append("Каталог " + file.getName() + " создан в " + file.getPath() + "\n");
+        } else {
+            log.append("Каталог " + file.getName() + " НЕ создан в " + file.getPath() + "\n");
         }
     }
 }
