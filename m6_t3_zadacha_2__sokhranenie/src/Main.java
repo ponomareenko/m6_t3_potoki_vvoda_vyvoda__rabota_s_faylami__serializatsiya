203172912f1988641efff322df003b3d747de7a0
Danil Ponomarenko <el.pochtadp@gmail.com>
2026-07-01 12:19:53 +0300

    Update Main.java

--- m6_t3_zadacha_2__sokhranenie/src/Main.java
+++ m6_t3_zadacha_2__sokhranenie/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         GameProgress playerTwo = new GameProgress(64, 45, 8, 22.5);
         GameProgress playerThree = new GameProgress(20, 36, 2, 10.2);
 
-        String myPath = "/Users/ponomarenko/learning/actual/java/netology/classwork/mod_6_java_core/t3_potoki_vvoda_vyvoda_rabota_s_faylami_serializatsiya/m6_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/m6_t3_zadacha_1__ustanovka/Games/savegames";
+        String myPath = "C:/Users/d.ponomarenko/work/napravleniya_raboty/my_personal_dannue/java/m4_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/ver3/m6_t3_potoki_vvoda_vyvoda__rabota_s_faylami__serializatsiya/m6_t3_zadacha_1__ustanovka/Games/savegames";
 
         Serializator serializator = new Serializator();
 
